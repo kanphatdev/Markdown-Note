2bad99c40b1c73b153e3427cbbde22a5fa0a04d7
kanphatratrazer <kanphatratrazer28@gmail.com>
2025-03-01 22:28:01 +0700

    adding Routing

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,5 +10,22 @@ export default {
   plugins: [
     require('daisyui'),
   ],
-}
-
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#ff0097",
+          "secondary": "#00e7ff",
+          "accent": "#5edd00",
+          "neutral": "#000608",
+          "base-100": "#f0ffff",
+          "info": "#00e6ff",
+          "success": "#00ba73",
+          "warning": "#c95b00",
+          "error": "#ff5f6a",
+        },
+      },
+    ],
+  },
+};
+ 
